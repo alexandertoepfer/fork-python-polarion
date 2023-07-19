7f3f06ca6a0a25d39a10aeeb11f2f9828d693950
Alexander Töpfer <74869642+alexandertoepfer@users.noreply.github.com>
2023-07-19 18:04:46 +0200

    Update custom_fields.py

--- polarionatofork/base/custom_fields.py
+++ polarionatofork/base/custom_fields.py
@@ -1,6 +1,6 @@
 from abc import ABC
 
-from polarion.base.polarion_object import PolarionObject
+from polarionatofork.base.polarion_object import PolarionObject
 
 
 class CustomFields(PolarionObject, ABC):
